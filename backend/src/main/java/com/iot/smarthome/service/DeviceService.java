98604cebb4f9fa4fcbf5464f6b9d01dbe894ef6d
oussamaelmessaoudi <oussama.elmessaoudi.39@edu.uiz.ac.ma>
2025-10-01 14:04:59 +0100

    feat: Adding business logic/ service layer for Device creation and certificates generation

--- backend/src/main/java/com/iot/smarthome/service/DeviceService.java
+++ backend/src/main/java/com/iot/smarthome/service/DeviceService.java
@@ -1,4 +1,28 @@
 package com.iot.smarthome.service;
 
+
+import com.iot.smarthome.dto.DeviceRegistrationRequest;
+import com.iot.smarthome.dto.DeviceRegistrationResponse;
+import com.iot.smarthome.repository.DeviceRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class DeviceService {
+    private final DeviceRepository deviceRepository;
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Transactional
+    public DeviceRegistrationResponse registerDevice(DeviceRegistrationRequest request, UUID userId){
+        log.info("Registering device for user {}", userId);
+
+
+    }
 }
