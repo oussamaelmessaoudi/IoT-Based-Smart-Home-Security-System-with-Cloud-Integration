3a9daa75ff454ffafc3b661a1f0277d61c88944a
oussamaelmessaoudi <oussama.elmessaoudi.39@edu.uiz.ac.ma>
2025-10-01 14:04:03 +0100

    feat: Adding repository for Device

--- backend/src/main/java/com/iot/smarthome/repository/DeviceRepository.java
+++ backend/src/main/java/com/iot/smarthome/repository/DeviceRepository.java
@@ -1,4 +1,11 @@
 package com.iot.smarthome.repository;
 
-public class DeviceRepository {
+import com.iot.smarthome.model.Device;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface DeviceRepository extends JpaRepository<UUID, Device> {
 }
